7b57ec95ec0e076d6d2693d338777c524cf5ac5d
wartakita <161806668+wartakita@users.noreply.github.com>
2024-12-22 10:18:28 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,61 +1,81 @@
-function _0x26c3() {
-  const _0x2c4f87 = ["video-play", "hing sched", ".m3u8", "kickOff", "includes", "then", "\n         ", "tor", "/span>\n   ", "kers.dev/h", "10028655pAjdmE", "catch", "DiOhu", "matchTitle", "   <button", "ive/ballba", "LaElr", "7162458Fevarj", "add", "Sfbjj", "', '", "innerHTML", "style", "126305LSiyLe", "934846wFNvKd", "filterInpu", "scrollTo", "-child(4)", "qiYce", 'nclick="to', "k21xyz.wor", "nabRc", "3567411bjxlZy", "torAll", "addEventLi", "      <spa", "none", "kJDgM", "match-item", "pBtn", "ule:", "documentEl", 'tch-btn" o', "     <span", "twyHQ", "y1nm.hnyon", "pan><stron", "29594696kgOObf", "opacity", "dataset", "     <div ", "https://da", "-pencuri-s", "json", "ntonMatch(", "730666qyyWRp", "100%", "JjTdc", "ule.json", "myRfA", "input", "kyrxg", "scrollTop", "KYGFH", "o/tv.githu", "View", "ttps://pla", "MRjdG", "        <s", "https://wa", "scrollInto", "toLowerCas", "smooth", "dPNlv", "</span>\n  ", "date", "createElem", "display", 'class="mat', "AvEJX", "video-cont", "Error fetc", "kXLPa", "bYMaN", "block", "o span:nth", "oOeQZ", ' class="ma', "188JtUqEI", "Ikabx", "   <i clas", "')\">\n     ", "nYnPC", "a.github.i", "-child(2)", ".no-result", "iZLMa", 'play"></i>', 'ch-info">\n', "appendChil", "ainer", "stener", "setup", "querySelec", "  </div>\n ", "classList", "league", "onscroll", "gshun.cn/l", "</strong><", "match-titl", "scheduleLi", "match", "ement", "vomJn", "JbNCw", ".match-ite", " Watch\n   ", "//wartakit", ".gd/NfRUC", "center", "getElement", "scrollToTo", "JIumF", "videoId", " </button>", "          ", "body", "textConten", "waTrK", "error", "b.io/sched", "rning.hayo", "ent", "ById", "value", "forEach", ".match-inf", 's="fas fa-'];
-  _0x26c3 = function () {
-    return _0x2c4f87;
-  };
-  return _0x26c3();
-}
-const _0x5ece98 = _0x4723;
-(function (_0x5513af, _0x3f3074) {
-  const _0xd32bf7 = _0x4723, _0x3d3bec = _0x5513af();
-  while (true) {
-    try {
-      const _0x557840 = -parseInt(_0xd32bf7(532)) / 1 + parseInt(_0xd32bf7(563)) / 2 + parseInt(_0xd32bf7(540)) / 3 + parseInt(_0xd32bf7(457)) / 4 * (parseInt(_0xd32bf7(531)) / 5) + parseInt(_0xd32bf7(525)) / 6 + parseInt(_0xd32bf7(518)) / 7 + -parseInt(_0xd32bf7(555)) / 8;
-      if (_0x557840 === _0x3f3074) break; else _0x3d3bec.push(_0x3d3bec.shift());
-    } catch (_0x17029b) {
-      _0x3d3bec.push(_0x3d3bec.shift());
+[{
+        "date": "2024-12-22",
+        "match": "Sacramento Kings vs Los Angeles Lakers",
+        "kickOff": "06:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "35180"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Orlando Magic vs Miami Heat",
+        "kickOff": "07:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "30690"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Atlanta Hawks vs Memphis Grizzlies",
+        "kickOff": "07:30",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "40480"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Brooklyn Nets vs Utah Jazz",
+        "kickOff": "07:30",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "37950"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Chicago Bulls vs Boston Celtics",
+        "kickOff": "08:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "33260"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Cleveland Cavaliers vs Philadelphia 76ers",
+        "kickOff": "08:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "35950"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Milwaukee Bucks vs Washington Wizards",
+        "kickOff": "08:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "27160"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Minnesota Timberwolves vs Golden State Warriors",
+        "kickOff": "08:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "27730"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "New Orleans Pelicans vs New York Knicks",
+        "kickOff": "08:00",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "24440"
+    },
+    {
+        "date": "2024-12-22",
+        "match": "Dallas Mavericks vs Los Angeles Clippers",
+        "kickOff": "08:30",
+        "league": "NBA",
+        "category": "Basketball",
+        "videoId": "23020"
     }
-  }
-}(_0x26c3, 733962), fetch(_0x5ece98(487) + _0x5ece98(462) + _0x5ece98(572) + _0x5ece98(500) + _0x5ece98(566))[_0x5ece98(513)](_0x53894a => _0x53894a[_0x5ece98(561)]())[_0x5ece98(513)](_0x30572a => {
-  const _0x208dfd = _0x5ece98, _0x3ea884 = {qiYce: _0x208dfd(546), iZLMa: _0x208dfd(506) + _0x208dfd(454) + _0x208dfd(463), myRfA: _0x208dfd(506) + _0x208dfd(454) + _0x208dfd(535), dPNlv: _0x208dfd(544), vomJn: _0x208dfd(485) + "m", Sfbjj: _0x208dfd(464) + "s", oOeQZ: _0x208dfd(453), kJDgM: _0x208dfd(480) + "st", LaElr: _0x208dfd(533) + "t", kyrxg: _0x208dfd(568)}, _0x549fbb = document[_0x208dfd(490) + _0x208dfd(503)](_0x3ea884[_0x208dfd(545)]);
-  _0x30572a[_0x208dfd(505)](_0x29096a => {
-    const _0xa69c1b = _0x208dfd, _0x463209 = document[_0xa69c1b(445) + _0xa69c1b(502)]("li");
-    _0x463209[_0xa69c1b(474)][_0xa69c1b(526)](_0x3ea884[_0xa69c1b(536)]), _0x463209[_0xa69c1b(557)][_0xa69c1b(493)] = _0x29096a[_0xa69c1b(493)], _0x463209[_0xa69c1b(557)][_0xa69c1b(521)] = _0x29096a[_0xa69c1b(521)], _0x463209[_0xa69c1b(529)] = _0xa69c1b(514) + _0xa69c1b(495) + _0xa69c1b(558) + _0xa69c1b(447) + _0xa69c1b(467) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(437) + _0xa69c1b(554) + "g>" + _0x29096a[_0xa69c1b(444)] + (_0xa69c1b(478) + _0xa69c1b(516) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(551) + ">") + _0x29096a[_0xa69c1b(481)] + (_0xa69c1b(443) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(543) + "n>") + _0x29096a[_0xa69c1b(511)] + (_0xa69c1b(443) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(543) + "n>") + _0x29096a[_0xa69c1b(475)] + (_0xa69c1b(443) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(473) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(522) + _0xa69c1b(456) + _0xa69c1b(550) + _0xa69c1b(537) + _0xa69c1b(562) + "'") + _0x29096a[_0xa69c1b(493)] + _0xa69c1b(528) + _0x29096a[_0xa69c1b(481)] + (_0xa69c1b(460) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(459) + _0xa69c1b(507) + _0xa69c1b(466) + _0xa69c1b(486) + _0xa69c1b(495) + _0xa69c1b(495) + _0xa69c1b(494) + _0xa69c1b(514) + _0xa69c1b(495) + " "), _0x549fbb[_0xa69c1b(468) + "d"](_0x463209);
-  });
-  const _0x3603c5 = document[_0x208dfd(490) + _0x208dfd(503)](_0x3ea884[_0x208dfd(524)]);
-  _0x3603c5[_0x208dfd(542) + _0x208dfd(470)](_0x3ea884[_0x208dfd(569)], function () {
-    const _0x1377ea = _0x208dfd, _0x335596 = _0x3603c5[_0x1377ea(504)][_0x1377ea(440) + "e"](), _0x1be0e0 = document[_0x1377ea(472) + _0x1377ea(541)](_0x3ea884[_0x1377ea(483)]);
-    let _0x28930e = false;
-    _0x1be0e0[_0x1377ea(505)](_0x29b40e => {
-      const _0x3c1602 = _0x1377ea, _0x198387 = _0x29b40e[_0x3c1602(472) + _0x3c1602(515)](_0x3ea884[_0x3c1602(465)])[_0x3c1602(497) + "t"][_0x3c1602(440) + "e"](), _0x55b704 = _0x29b40e[_0x3c1602(472) + _0x3c1602(515)](_0x3ea884[_0x3c1602(567)])[_0x3c1602(497) + "t"][_0x3c1602(440) + "e"]();
-      _0x198387[_0x3c1602(512)](_0x335596) || _0x55b704[_0x3c1602(512)](_0x335596) ? (_0x29b40e[_0x3c1602(530)][_0x3c1602(446)] = "", _0x28930e = true) : _0x29b40e[_0x3c1602(530)][_0x3c1602(446)] = _0x3ea884[_0x3c1602(442)];
-    }), document[_0x1377ea(472) + _0x1377ea(515)](_0x3ea884[_0x1377ea(527)])[_0x1377ea(530)][_0x1377ea(446)] = _0x28930e ? _0x3ea884[_0x1377ea(442)] : _0x3ea884[_0x1377ea(455)];
-  });
-})[_0x5ece98(519)](_0x93a793 => console[_0x5ece98(499)](_0x5ece98(450) + _0x5ece98(509) + _0x5ece98(548), _0x93a793)));
-function _0x4723(_0x2536e5, _0x33fc9e) {
-  const _0x48ea65 = _0x26c3();
-  return _0x4723 = function (_0x2bbd72, _0x17c6e8) {
-    _0x2bbd72 = _0x2bbd72 - 434;
-    let _0x140ca0 = _0x48ea65[_0x2bbd72];
-    return _0x140ca0;
-  }, _0x4723(_0x2536e5, _0x33fc9e);
-}
-function tontonMatch(_0xd3ca2e, _0x241993) {
-  const _0x16c8a8 = _0x5ece98, _0x250276 = {DiOhu: _0x16c8a8(449) + _0x16c8a8(469), JbNCw: _0x16c8a8(479) + "e", JIumF: _0x16c8a8(508) + "er", nYnPC: function (_0x20eeab, _0x47de37) {
-    return _0x20eeab(_0x47de37);
-  }, bYMaN: _0x16c8a8(559) + _0x16c8a8(488), kXLPa: _0x16c8a8(564), Ikabx: _0x16c8a8(441), JjTdc: _0x16c8a8(489)}, _0xc9022d = document[_0x16c8a8(490) + _0x16c8a8(503)](_0x250276[_0x16c8a8(520)]), _0x39d837 = document[_0x16c8a8(490) + _0x16c8a8(503)](_0x250276[_0x16c8a8(484)]), _0x34f7bd = document[_0x16c8a8(490) + _0x16c8a8(503)](_0x250276[_0x16c8a8(492)]);
-  _0x39d837[_0x16c8a8(497) + "t"] = _0x241993, _0xc9022d[_0x16c8a8(530)][_0x16c8a8(556)] = "1", _0x250276[_0x16c8a8(461)](jwplayer, _0x250276[_0x16c8a8(492)])[_0x16c8a8(471)]({file: _0x16c8a8(438) + _0x16c8a8(501) + _0x16c8a8(560) + _0x16c8a8(538) + _0x16c8a8(517) + _0x16c8a8(435) + _0x16c8a8(553) + _0x16c8a8(477) + _0x16c8a8(523) + "r_" + _0xd3ca2e + _0x16c8a8(510), image: _0x250276[_0x16c8a8(452)], width: _0x250276[_0x16c8a8(451)], height: _0x250276[_0x16c8a8(451)], autostart: true}), _0xc9022d[_0x16c8a8(439) + _0x16c8a8(434)]({behavior: _0x250276[_0x16c8a8(458)], block: _0x250276[_0x16c8a8(565)]});
-}
-window[_0x5ece98(476)] = function () {
-  const _0x330b11 = _0x5ece98, _0x55218f = {nabRc: _0x330b11(491) + _0x330b11(547), waTrK: function (_0x442645, _0x33f935) {
-    return _0x442645 > _0x33f935;
-  }, KYGFH: function (_0x3eb71, _0x1d59d2) {
-    return _0x3eb71 > _0x1d59d2;
-  }, MRjdG: _0x330b11(453), twyHQ: _0x330b11(544)};
-  var _0x549163 = document[_0x330b11(490) + _0x330b11(503)](_0x55218f[_0x330b11(539)]);
-  _0x55218f[_0x330b11(498)](document[_0x330b11(496)][_0x330b11(570)], 100) || _0x55218f[_0x330b11(571)](document[_0x330b11(549) + _0x330b11(482)][_0x330b11(570)], 100) ? _0x549163[_0x330b11(530)][_0x330b11(446)] = _0x55218f[_0x330b11(436)] : _0x549163[_0x330b11(530)][_0x330b11(446)] = _0x55218f[_0x330b11(552)];
-};
-function scrollToTop() {
-  const _0xdcf4ed = _0x5ece98, _0x1ea1c0 = {AvEJX: _0xdcf4ed(441)};
-  window[_0xdcf4ed(534)]({top: 0, behavior: _0x1ea1c0[_0xdcf4ed(448)]});
-}
+]
