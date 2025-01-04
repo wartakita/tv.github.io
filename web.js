d5d50dcbb156d25f2d8d40ca97d839aaeb087256
wartakita <161806668+wartakita@users.noreply.github.com>
2025-01-04 16:57:58 +0700

    Update web.js

--- web.js
+++ web.js
@@ -1,14 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Membuat elemen iframe
-    const iframe = document.createElement('iframe');
-    iframe.id = 'main-iframe';
-    iframe.src = 'https://player.kick.com/pisangg';
-    iframe.frameBorder = '0';
-    iframe.sandbox = 'allow-scripts allow-same-origin';
-    iframe.allowFullscreen = true;
-    iframe.title = 'Iframe Konten Utama';
-
-    // Menambahkan iframe ke kontainer
-    const iframeContainer = document.getElementById('iframe-container');
-    iframeContainer.appendChild(iframe);
-});
+const _0x518397 = _0x26d1;
+function _0x3026() {
+  const _0x159e38 = ["1632ERkZxy", "Loaded", "DOMContent", "ten Utama", "frameBorde", "addEventLi", "same-origi", "title", "30pBBPCB", "pts allow-", "allow-scri", "95316lTAboN", "allowFulls", "sOgpa", "tainer", "RVTpS", "creen", "1237576Xwibtp", "iframe", "14322JRECZO", "ById", "AlIPQ", "ayer.kick.", "1501872EgNWNp", "stener", "27teevqz", "FLIAx", "eDYWk", "Iframe Kon", "466331iEhzbR", "iframe-con", "9677190HuHDqG", "getElement", "main-ifram", "ent", "2429795IBQRcj", "createElem", "appendChil", "src", "https://pl", "fHNyq", "com/pisang", "sandbox"];
+  _0x3026 = function () {
+    return _0x159e38;
+  };
+  return _0x3026();
+}
+function _0x26d1(_0xb2617f, _0x537dbd) {
+  const _0x175543 = _0x3026();
+  return _0x26d1 = function (_0x41f234, _0x46569b) {
+    _0x41f234 = _0x41f234 - 298;
+    let _0x4c427c = _0x175543[_0x41f234];
+    return _0x4c427c;
+  }, _0x26d1(_0xb2617f, _0x537dbd);
+}
+(function (_0x3409bb, _0x4eb9f9) {
+  const _0x1e0896 = _0x26d1, _0x38af21 = _0x3409bb();
+  while (true) {
+    try {
+      const _0x1b1607 = -parseInt(_0x1e0896(311)) / 1 + -parseInt(_0x1e0896(333)) / 2 * (parseInt(_0x1e0896(336)) / 3) + -parseInt(_0x1e0896(305)) / 4 + -parseInt(_0x1e0896(317)) / 5 + parseInt(_0x1e0896(325)) / 6 * (-parseInt(_0x1e0896(301)) / 7) + -parseInt(_0x1e0896(299)) / 8 + parseInt(_0x1e0896(307)) / 9 * (parseInt(_0x1e0896(313)) / 10);
+      if (_0x1b1607 === _0x4eb9f9) break; else _0x38af21.push(_0x38af21.shift());
+    } catch (_0x4a54d0) {
+      _0x38af21.push(_0x38af21.shift());
+    }
+  }
+}(_0x3026, 387610), document[_0x518397(330) + _0x518397(306)](_0x518397(327) + _0x518397(326), function () {
+  const _0xcf5bc8 = _0x518397, _0xb5c00c = {RVTpS: _0xcf5bc8(300), FLIAx: _0xcf5bc8(315) + "e", AlIPQ: _0xcf5bc8(321) + _0xcf5bc8(304) + _0xcf5bc8(323) + "g", sOgpa: _0xcf5bc8(335) + _0xcf5bc8(334) + _0xcf5bc8(331) + "n", eDYWk: _0xcf5bc8(310) + _0xcf5bc8(328), fHNyq: _0xcf5bc8(312) + _0xcf5bc8(339)}, _0x447657 = document[_0xcf5bc8(318) + _0xcf5bc8(316)](_0xb5c00c[_0xcf5bc8(340)]);
+  _0x447657.id = _0xb5c00c[_0xcf5bc8(308)], _0x447657[_0xcf5bc8(320)] = _0xb5c00c[_0xcf5bc8(303)], _0x447657[_0xcf5bc8(329) + "r"] = "0", _0x447657[_0xcf5bc8(324)] = _0xb5c00c[_0xcf5bc8(338)], _0x447657[_0xcf5bc8(337) + _0xcf5bc8(298)] = true, _0x447657[_0xcf5bc8(332)] = _0xb5c00c[_0xcf5bc8(309)];
+  const _0x2a398e = document[_0xcf5bc8(314) + _0xcf5bc8(302)](_0xb5c00c[_0xcf5bc8(322)]);
+  _0x2a398e[_0xcf5bc8(319) + "d"](_0x447657);
+}));
